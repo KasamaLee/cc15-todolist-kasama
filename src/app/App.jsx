374f414660f4ae3185bc295dd16a7ee0b9d50afb
KasamaLee <k.kasama.lee@gmail.com>
2023-08-30 12:13:03 +0700

    refractor : create listItem component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,7 @@
 import './App.scss';
 import Header from '../components/Header.jsx';
+import ListItem from '../components/ListItem';
+import { FaCalendar, FaCalendarAlt, FaInbox } from 'react-icons/fa';
 
 function App() {
   return (
@@ -7,7 +9,44 @@ function App() {
       <div className="todo__header">
         <Header />
       </div>
-      <div className="todo__sidebar">SideBar</div>
+
+      <div className="todo__sidebar">
+        <aside className='sidebar'>
+
+          <section className='sidebar__category'>
+            <ul className='list'>
+              {/* <li className='list__item'> 
+                <FaInbox className='list__item__icon' />
+                <p className='list__item__text'>Inbox</p>
+              </li>
+              <li className='list__item'> 
+                <FaCalendar className='list__item__icon' />
+                <p className='list__item__text'>Today</p>
+              </li>
+              <li className='list__item'> 
+                <FaCalendarAlt className='list__item__icon' />
+                <p className='list__item__text'>Next 7 Days</p>
+              </li> */}
+
+              <ListItem
+                text='Inbox'
+                icon={<FaInbox className='list__item__icon' />}
+              />
+              <ListItem
+                text='Today'
+                icon={<FaCalendar className='list__item__icon' />}
+              />
+              <ListItem
+                text='Next 7 days'
+                icon={<FaCalendarAlt className='list__item__icon' />}
+              />
+            </ul>
+          </section>
+
+          <section className='sidebar__category'></section>
+        </aside>
+      </div>
+
       <div className="todo_content">TodoContent</div>
     </div>
   );
